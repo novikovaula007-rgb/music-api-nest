876544a1058b30835581cd13c334ddb2659130d2
ulyana <novikova.ula.007@gmail.com>
2026-04-24 06:51:57 +0300

    add CRUD operations for track

---
+++ src/tracks/tracks.controller.ts
@@ -0,0 +1,33 @@
+import {Body, Controller, Delete, Get, Param, Post, Query} from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Track} from "../schemas/track.schema";
+import {Model} from "mongoose";
+import {CreateTrackDto} from "./create-track.dto";
+
+@Controller('tracks')
+export class TracksController {
+    constructor(
+        @InjectModel(Track.name) private trackModel: Model<Track>
+    ) {}
+
+    @Get()
+    async findAll(@Query('album') albumId?: string) {
+        const filter = albumId ? {album: albumId} : {};
+        return this.trackModel.find(filter);
+    }
+
+    @Post()
+    async create(@Body() createTrackDto: CreateTrackDto) {
+        const track = new this.trackModel({
+            title: createTrackDto.title,
+            duration: createTrackDto.duration,
+            album: createTrackDto.album
+        });
+
+        return track.save();
+    }
+
+    @Delete()
+    async deleteOne(@Param('id') id: string) {
+        return this.trackModel.findByIdAndDelete(id);
+    }}
